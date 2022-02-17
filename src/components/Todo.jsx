bf5a488783d3eac4f846231685f1568d953a1331
David Skotte <david.skotte@gmail.com>
2022-02-17 16:54:56 +0100

    fixat input + delete knapp

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import './css/todo.module.css';
 
-const Todo = ( {text} ) => {
+const Todo = ( {activities, setActivities, todo, text, id} ) => {
+
+    const deleteHandler = (text) => {
+        const newActivities = [...activities].filter(
+            (activities) => activities.id !== id);
+            setActivities(newActivities);
+            console.log(newActivities);
+    }
+
+    const completeHandler({activities})
+      const currentActivity = activities.find((activities) => {
+          return activities.id === activities.id;
+      })
+
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
